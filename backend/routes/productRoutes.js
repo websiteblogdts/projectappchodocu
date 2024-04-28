520bea79b194cb28e0d110805275eb0b3620b47c
threesion <threesion@gmail.com>
2024-04-29 01:17:54 +0700

    phân quyền cho chức năng

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,15 +7,20 @@ const authMiddleware = require('../middlewares/authMiddleware');
 router.get('/category/:id', categoryController.getCategoryById);
 router.get('/category', categoryController.getAllCategories);
 
-router.get('/productlistbyuser', authMiddleware,  productController.getAllProductsByUser);
 
 router.get('/',  productController.getAllProducts);
 
-router.get('/:productId', productController.getProductById);
+// router.get('/:productId', productController.getProductById);
 
 router.get('/helloson', productController.getRoutes);
 
 router.use(authMiddleware);
+
+router.get('/productlistbyuser', authMiddleware,  productController.getAllProductsByUser);
+
+router.get('/productdaduyet', authMiddleware,  productController.getAllProductsByUserApproved);
+
+router.get('/:productId', authMiddleware, productController.getProductById);
 
 router.put('/editproduct/:productId',authMiddleware, productController.updateProductById);
 
